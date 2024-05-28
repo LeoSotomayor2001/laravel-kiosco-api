fbc854b5e8ca3e2034cace27abcb8a00f144a37f
Leodomi <Leodomi15@gmail.com>
2024-05-27 21:25:26 -0400

    creando el endpoint de producto

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', )->middleware('auth:sanctum');
 Route::apiResource('/categorias',CategoriaController::class);
+Route::apiResource('/productos',ProductoController::class);
